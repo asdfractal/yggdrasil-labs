3701b0b71dd6f58aeb33d1446e04833be2881be1
James Gregory <james.fractal@protonmail.com>
2020-07-30 15:54:49 +1000

    change default secret key and put in env variable

--- yggdrasil_labs/settings.py
+++ yggdrasil_labs/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+ce!t6b+f8*4@wt@q$d7#p$cf3d(3h99ymp_!42yn9zyzfar4_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
