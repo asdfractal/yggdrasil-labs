8a5ed0615b8bf2f9d9ab3aaa16a1f2e12665bdd4
James Gregory <james.fractal@protonmail.com>
2020-08-05 00:47:34 +1000

    update view to show products and test in template

--- products/views.py
+++ products/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def products(request):
     """
     Returns a view with all products displayed.
     """
-    return render(request, "products.html")
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+
+    return render(request, "products.html", context)
