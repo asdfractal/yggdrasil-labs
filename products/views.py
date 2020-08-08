18e88c7bd59d1b6e6acc17301ad431781fb51608
James Gregory <james.fractal@protonmail.com>
2020-08-08 15:04:11 +1000

    test review in product details view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from reviews.models import Review
 from .models import Product, Category
 
 
@@ -6,22 +7,25 @@ def products(request):
     """
     Returns a view with all products displayed.
     """
-    products = Product.objects.all()
+    all_products = Product.objects.all()
     categories = Category.objects.all()
     context = {
-        "products": products,
+        "products": all_products,
         "categories": categories,
     }
 
     return render(request, "products.html", context)
+
 
 def product_detail(request, product_id):
     """
     Returns a view with individual product details.
     """
     product = get_object_or_404(Product, pk=product_id)
+    review = get_object_or_404(Review, pk=product_id)
     context = {
         "product": product,
+        "review": review,
     }
 
-    return render(request, 'product_detail.html', context)
+    return render(request, "product_detail.html", context)
