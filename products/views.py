680244c9c083c84991f6dd0a514052b456d3db50
James Gregory <james.fractal@protonmail.com>
2020-08-09 00:14:48 +1000

    create form for adding review and link - not fully working

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
 from reviews.models import Review
+from reviews.forms import ReviewForm
+from profiles.models import UserProfile
 from .models import Product, Category
 
 
@@ -35,11 +38,30 @@ def product_reviews(request, product_id):
     """
     Returns a view with all the reviews for a specific product.
     """
+    user = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+    print(user)
+    print(user.id)
+    form = ReviewForm(user)
+    if request.method == "POST":
+        form_data = {
+            "user_profle": user,
+            "product": product,
+            "review_content": request.POST["review_content"],
+        }
+        form = ReviewForm(user, form_data)
+        print(form_data)
+        if form.is_valid():
+            review = form.save(product_id)
+            review.save()
+
     product = get_object_or_404(Product, pk=product_id)
     reviews = Review.objects.filter(product=product_id)
+
     context = {
         "product": product,
         "reviews": reviews,
+        "form": form,
     }
 
     return render(request, "product-reviews.html", context)
