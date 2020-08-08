837770c6472606e916d1c57919f9f398a2023677
James Gregory <james.fractal@protonmail.com>
2020-08-08 12:11:37 +1000

    create basic profile view, template and urls

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+from .models import UserProfile
+
+
+def profile(request, profile_id):
+    """
+    Display the users profile.
+    """
+    user_profile = get_object_or_404(UserProfile, user=request.user, id=profile_id)
+
+    context = {
+        "profile": user_profile,
+    }
+
+    return render(request, "dashboard.html", context)
--- yggdrasil_labs/urls.py
+++ yggdrasil_labs/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from pages.views import index
-from products.views import products
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+from pages.views import index
+
+# from products.views import products
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", index, name="index"),
     path("products/", include("products.urls")),
+    path("u/", include("profiles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
