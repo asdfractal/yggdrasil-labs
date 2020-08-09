19a495f1531fe4876ae7c1827e0ddac8868ba18a
James Gregory <james.fractal@protonmail.com>
2020-08-10 00:07:24 +1000

    rename functions for clarity and add docs

--- products/static/products/js/scripts.js
+++ products/static/products/js/scripts.js
@@ -6,14 +6,20 @@ const reviewContent = $('#id_review_content')
 const reviewSubmit = $('#reviewSubmitButton')
 const editReview = $('#editReview')
 
-const enableEditReview = () => {
+/**
+ * Enables form to add or edit review.
+ */
+const enableReviewForm = () => {
 	reviewContent.removeAttr('disabled')
 	reviewSubmit.parent().addClass('d-block')
 	newReviewForm.addClass('d-block')
 	editReview.attr('id', 'cancelEditReview').removeClass('far fa-edit').addClass('fas fa-times')
 }
 
-const disableEditReview = () => {
+/**
+ * Disables form to add or edit review.
+ */
+const disableReviewForm = () => {
 	reviewContent.attr('disabled', 'true')
 	reviewSubmit.parent().removeClass('d-block')
 	newReviewForm.removeClass('d-block')
@@ -22,9 +28,9 @@ const disableEditReview = () => {
 
 $(document).ready(function () {
 	$(document).on('click', '#editReview', function () {
-		enableEditReview()
+		enableReviewForm()
 	})
 	$(document).on('click', '#cancelEditReview', function () {
-		disableEditReview()
+		disableReviewForm()
 	})
 })
