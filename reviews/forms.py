f2c14895fe05d9bc65f965f0ff86578aba85e0ae
James Gregory <james.fractal@protonmail.com>
2020-08-09 18:24:43 +1000

    add more styling for review page and forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -8,7 +8,7 @@ class ReviewForm(forms.ModelForm):
         fields = ["review_content"]
         widgets = {
             "review_content": forms.Textarea(
-                attrs={"disabled": True, "class": "review-form-textarea", "rows": 5,}
+                attrs={"disabled": True, "class": "review-form-textarea", "rows": 10,}
             ),
         }
 
