8dc2b34f42fe7ab7083c0afe3bda377799781d40
James Gregory <james.fractal@protonmail.com>
2020-08-09 18:26:09 +1000

    add static js for products app and extra scripts block in base template to load it

---
+++ products/static/products/js/scripts.js
@@ -0,0 +1,27 @@
+/*jshint esversion: 10 */
+/*jshint -W033 */
+
+const reviewContent = $('#id_review_content')
+const reviewSubmit = $('#reviewSubmitButton')
+const editReview = $('#editReview')
+
+const enableEditReview = () => {
+	reviewContent.removeAttr('disabled')
+	reviewSubmit.parent().addClass('d-block')
+	editReview.attr('id', 'cancelEditReview').removeClass('far fa-edit').addClass('fas fa-times')
+}
+
+const disableEditReview = () => {
+	reviewContent.attr('disabled', 'true')
+	reviewSubmit.parent().removeClass('d-block')
+	$('#cancelEditReview').attr('id', 'editReview').removeClass('fas fa-times').addClass('far fa-edit')
+}
+
+$(document).ready(function () {
+	$(document).on('click', '#editReview', function () {
+		enableEditReview()
+	})
+	$(document).on('click', '#cancelEditReview', function () {
+		disableEditReview()
+	})
+})
