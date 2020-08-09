ecaadbfb52a868097db2753b6eac7f710604140e
James Gregory <james.fractal@protonmail.com>
2020-08-09 19:01:42 +1000

    add form enabling for new review

--- products/static/products/js/scripts.js
+++ products/static/products/js/scripts.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 10 */
 /*jshint -W033 */
 
+const newReviewForm = $('#newUserReview > form')
 const reviewContent = $('#id_review_content')
 const reviewSubmit = $('#reviewSubmitButton')
 const editReview = $('#editReview')
@@ -8,12 +9,14 @@ const editReview = $('#editReview')
 const enableEditReview = () => {
 	reviewContent.removeAttr('disabled')
 	reviewSubmit.parent().addClass('d-block')
+	newReviewForm.addClass('d-block')
 	editReview.attr('id', 'cancelEditReview').removeClass('far fa-edit').addClass('fas fa-times')
 }
 
 const disableEditReview = () => {
 	reviewContent.attr('disabled', 'true')
 	reviewSubmit.parent().removeClass('d-block')
+	newReviewForm.removeClass('d-block')
 	$('#cancelEditReview').attr('id', 'editReview').removeClass('fas fa-times').addClass('far fa-edit')
 }
 
