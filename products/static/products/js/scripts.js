e536ac9b397ff85fa8a6ee4bb397b031b74adbd9
James Gregory <james.fractal@protonmail.com>
2020-08-12 12:00:33 +1000

    reorganise display and filtering on products page, add js filtering

--- products/static/products/js/scripts.js
+++ products/static/products/js/scripts.js
@@ -5,6 +5,8 @@ const newReviewForm = $('#newUserReview > form')
 const reviewContent = $('#id_review_content')
 const reviewSubmit = $('#reviewSubmitButton')
 const editReview = $('#editReview')
+const categories = ['implant', 'app', 'upgrade']
+const filterActive = 'js-product-filter-button__active'
 
 /**
  * Enables form to add or edit review.
@@ -25,6 +27,47 @@ const disableReviewForm = () => {
 	newReviewForm.removeClass('d-block')
 	$('#cancelEditReview').attr('id', 'editReview').removeClass('fas fa-times').addClass('far fa-edit')
 }
+
+const getFilterId = () => {
+	return _this.attr('id')
+}
+
+const clearFilter = () => {
+	categories.forEach(cat => {
+		$(`.js-filter-${cat}`).removeClass('d-none')
+		$(`#${cat}`).removeClass(filterActive)
+		$('#clearFilter').addClass('text-muted')
+	})
+}
+
+const filterById = id => {
+	categories.forEach(cat => {
+		if (cat == id) {
+			$(`.js-filter-${cat}`).removeClass('d-none')
+			$(`#${cat}`).addClass(filterActive)
+		} else {
+			$(`.js-filter-${cat}`).addClass('d-none')
+			$(`#${cat}`).removeClass(filterActive)
+		}
+	})
+	$('#clearFilter').removeClass('text-muted')
+}
+
+document.querySelectorAll('.product-filter-button').forEach(element => {
+	element.addEventListener('click', function (e) {
+		e.preventDefault()
+		_this = $(this)
+		filterId = getFilterId()
+		filterById(filterId)
+	})
+})
+
+$('#clearFilter').click((e) => {
+	e.preventDefault();
+	clearFilter()
+})
+
+
 
 $(document).ready(function () {
 	$(document).on('click', '#editReview', function () {
