1285edf0bbea308b93b999eb21ee0255ded72bc4
James Gregory <james.fractal@protonmail.com>
2020-08-12 15:45:44 +1000

    write add to cart view and test with print

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.cart, name="cart"),
+    path("", views.view_cart, name="cart"),
+    path("add/<product_id>/", views.add_to_cart, name="add_to_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 
-def cart(request):
+def view_cart(request):
     """
     Display users shopping cart.
     """
 
     return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, product_id):
+    """
+    Add a specified product to the cart.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    cart = request.session.get("cart", {})
+
+    if product_id in list(cart.keys()):
+        messages.info(request, "That item is already in your cart.")
+        print("already in cart")
+        return redirect("product_detail", product_id)
+
+    cart[product_id] = 1
+    messages.success(request, f"Added {product.name} to your cart.")
+    print("added to cart")
+    request.session["cart"] = cart
+    return redirect("product_detail", product_id)
