da508fe6bd6351de6396efdc00b3497d523b905b
James Gregory <james.fractal@protonmail.com>
2020-08-14 09:51:41 +1000

    create and migrate booking, order and orderlineitem models

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import UserProfile
+
+
+class Booking(models.Model):
+    booking_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="bookings",
+    )
+    booking_time = models.DateTimeField(auto_now_add=False)
+
+    def __str__(self):
+        return self.booking_number
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,80 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+from products.models import Product
+from profiles.models import UserProfile
+from bookings.models import Booking
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    booking = models.OneToOneField(
+        Booking, null=True, blank=True, on_delete=models.SET_NULL, related_name="order",
+    )
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders",
+    )
+    full_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    state = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label="Country", null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+    )
+    original_cart = models.TextField(null=False, blank=False, default="")
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default="")
+
+    @staticmethod
+    def _generate_order_number():
+        """
+        Generate an order number using UUID.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.total_price = self.lineitems.aggregate(Sum("lineitems.product.price")) or 0
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Set the order number if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name="lineitems",
+    )
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"SKU {self.product.sku} on order {self.order.order_number}"
