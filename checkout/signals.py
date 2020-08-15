bbe97ad93348dbee77925f53228c8e405541bafe
James Gregory <james.fractal@protonmail.com>
2020-08-15 15:59:20 +1000

    update docstring in checkout signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -7,7 +7,7 @@ from .models import OrderLineItem
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem update/create
+    Update order total and shipping/booking status on lineitem update/create.
     """
     instance.order.update_total()
     instance.order.set_booking_required()
@@ -17,7 +17,7 @@ def update_on_save(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    Update order total on lineitem delete
+    Update order total and shipping/booking status on lineitem delete.
     """
     instance.order.update_total()
     instance.order.set_booking_required()
