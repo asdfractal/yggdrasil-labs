787eda24fee262eb441748d48cb6748dd154faee
James Gregory <james.fractal@protonmail.com>
2020-08-16 19:55:43 +1000

    update booking view and template styles, add script for functionality

--- bookings/static/bookings/js/scripts.js
+++ bookings/static/bookings/js/scripts.js
@@ -1,6 +1,24 @@
 /*jshint esversion: 10 */
 /*jshint -W033 */
 
+bookingForm = $('#bookingForm')
+changeBookingButton = $('#changeBookingButton')
+cancelChangeBookingButton = $('#cancelChangeBookingButton')
+
+changeBookingButton.click((e) => {
+	e.preventDefault()
+	bookingForm.removeClass('d-none')
+	changeBookingButton.parent().addClass('d-none')
+	cancelChangeBookingButton.removeClass('d-none')
+})
+
+cancelChangeBookingButton.click((e) => {
+	e.preventDefault()
+	bookingForm.addClass('d-none')
+	changeBookingButton.parent().removeClass('d-none')
+	cancelChangeBookingButton.addClass('d-none')
+})
+
 $(document).ready(function () {
 	$('.datepicker').datepicker();
 	$('.timepicker').timepicker();
--- bookings/views.py
+++ bookings/views.py
@@ -3,7 +3,7 @@ from django.utils.timezone import make_aware
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 
-from checkout.models import Order
+from checkout.models import Order, OrderLineItem
 from .models import Booking
 from .forms import BookingForm, BookingValidationForm
 
@@ -14,7 +14,14 @@ def create_booking(request, order_number):
     """
     order = get_object_or_404(Order, order_number=order_number)
     booking = get_object_or_404(Booking, order=order.id)
+    lineitems = OrderLineItem.objects.filter(order=order.id)
     form = BookingForm()
+    booking_item = None
+    for item in lineitems:
+        if item.product.booking_required:
+            booking_item = item.product.name
+            break
+
     if request.method == "POST":
         date_str = request.POST.get("booking_date", "")
         time_str = request.POST.get("booking_time", "")
@@ -32,8 +39,10 @@ def create_booking(request, order_number):
             aware_datetime = make_aware(datetime_obj)
             booking.booking_time = aware_datetime
             booking.save()
-            print("booking time updated")
-            return redirect(reverse("create_booking", args=[order_number]))
+            messages.success(
+                request, f"Booking created at {time_str} on {date_str}.",
+            )
+            return redirect(reverse("profile"))
         messages.error(
             request, "There was a problem with your selection, please try again."
         )
@@ -41,6 +50,7 @@ def create_booking(request, order_number):
 
     context = {
         "booking": booking,
+        "booking_item": booking_item,
         "form": form,
     }
     return render(request, "bookings/create_booking.html", context)
