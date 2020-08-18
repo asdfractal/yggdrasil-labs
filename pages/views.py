3ab7ccccd51cabd6dac507c0f1aed750b7d0a048
James Gregory <james.fractal@protonmail.com>
2020-08-18 14:50:46 +1000

    add urls to pages app and create contact and about view and templates

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,23 @@ def index(request):
     Renders the index view.
     """
     return render(request, "pages/index.html")
+
+
+def contact(request):
+    """
+    Renders a view with a form to contact the business.
+    """
+    context = {
+        "page_title": "Contact Us",
+    }
+    return render(request, "pages/contact.html", context)
+
+
+def about(request):
+    """
+    Renders a view with information about the business.
+    """
+    context = {
+        "page_title": "About Us",
+    }
+    return render(request, "pages/about.html", context)
--- yggdrasil_labs/urls.py
+++ yggdrasil_labs/urls.py
@@ -14,18 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
-from pages.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", index, name="index"),
+    path("", include("pages.urls")),
     path("products/", include("products.urls")),
     path("profile/", include("profiles.urls")),
     path("cart/", include("cart.urls")),
     path("checkout/", include("checkout.urls")),
     path("booking/", include("bookings.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
