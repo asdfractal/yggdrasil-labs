d60a72354e324d2cd6f03835b6ad31239291c885
James Gregory <james.fractal@protonmail.com>
2020-08-21 00:35:54 +1000

    add featured field and remove image field on product model

--- products/models.py
+++ products/models.py
@@ -10,9 +10,9 @@ class Product(models.Model):
     description = models.TextField(null=False, blank=False, default="")
     booking_required = models.BooleanField(default=False, null=False, blank=False)
     shipping_required = models.BooleanField(default=False, null=False, blank=False)
+    featured = models.BooleanField(default=False, null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
