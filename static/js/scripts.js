dc052953801324bc7bdc0751ecb79ff3e50c1b7b
James Gregory <james.fractal@protonmail.com>
2020-08-21 00:42:05 +1000

    add content to index page
    featured product carousel, mission statement placeholder text, and social proof carousel

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,3 +6,24 @@ $('.toast').toast('show')
 $('.toast-close').click(() => {
 	$('.toast').toast('hide')
 })
+
+$(document).ready(function () {
+	$('.index-featured').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
+	});
+	$('.index-social-proof').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 10000,
+	});
+});
