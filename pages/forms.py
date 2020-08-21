3b46555c43ece9358cbf22d7eb6cea5d0ce4d600
James Gregory <james.fractal@protonmail.com>
2020-08-21 13:03:20 +1000

    add docstrings

--- pages/forms.py
+++ pages/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 
 class ContactForm(forms.Form):
+    """
+    Form for user to contact business.
+    """
+
     content = forms.CharField(required=True, widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
