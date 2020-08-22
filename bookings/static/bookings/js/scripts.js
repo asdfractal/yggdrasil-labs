df26e3f6a0b015d7d44bed38dd0f1562cc76d33b
James Gregory <james.fractal@protonmail.com>
2020-08-22 21:55:54 +1000

    fix looping bug with buttons and clean up code

--- bookings/static/bookings/js/scripts.js
+++ bookings/static/bookings/js/scripts.js
@@ -7,6 +7,8 @@ const cancelChangeBookingButton = $('#cancelChangeBookingButton')
 const bookingDateInput = $("#id_booking_date")
 const bookingTimeInput = $('#id_booking_time')
 const baseUrl = "/api/booking/?booking_date__contains="
+const bookingErrors = $('#bookingErrors')
+const permanentTimes = ['09', '12', '15']
 let selectedDate = new Date()
 
 /* Dynamically displays and hides booking creation form */
@@ -24,8 +26,8 @@ cancelChangeBookingButton.click((e) => {
 	cancelChangeBookingButton.addClass('d-none')
 })
 
-document.querySelectorAll('.button-booking-time').forEach(el => {
-	el.addEventListener('click', function (e) {
+document.querySelectorAll('.button-booking-time').forEach(button => {
+	button.addEventListener('click', function (e) {
 		e.preventDefault()
 		bookingTimeInput.attr({ 'value': $(this).text() })
 		$('#bookingSubmitButton').removeAttr('disabled')
@@ -54,38 +56,43 @@ const checkDates = async (url, date) => {
 const processDates = async (url, date) => {
 	const data = await checkDates(url, date)
 	const bookingQuery = data.objects
+	const bookedTimes = []
+	bookingQuery.forEach(booking => {
+		time = booking.booking_time.slice(0, -6)
+		bookedTimes.push(time)
+	})
+	availableTimes = permanentTimes.filter(time => !bookedTimes.includes(time))
 	if (bookingQuery.length === 3) {
-		console.log('full today');
+		bookingErrors.text('No available bookings on this day. Please make another selection.')
 	} else if (bookingQuery.length === 0) {
-		console.log('no bookings');
-		document.querySelectorAll('.button-booking-time').forEach(el => {
-			el.disabled = false
+		document.querySelectorAll('.button-booking-time').forEach(button => {
+			button.disabled = false
 		})
 	} else {
-		bookingQuery.forEach(booking => {
-			time = booking.booking_time.slice(0, -6)
-			document.querySelectorAll('.button-booking-time').forEach(el => {
-				if (el.id != time) {
-					el.disabled = false
-				}
-			})
+		availableTimes.forEach(time => {
+			$(`#${time}`).removeAttr('disabled')
 		})
 	}
 }
 
 /**
- * Disables the time selection buttons before api response
+ * Disables the time selection buttons before api response and resets time input
+ * value to ensure an already booked time is not selected
  */
 const disableTimes = () => {
 	document.querySelectorAll('.button-booking-time').forEach(el => {
 		el.disabled = true
 	})
+	bookingTimeInput.attr({ 'value': '' })
 }
 
 $(document).ready(function () {
+	// Initialize datepicker and process input
 	bookingDateInput.datepicker({
 		minDate: 0, maxDate: "+1M", dateFormat: "dd M, yy", beforeShowDay: $.datepicker.noWeekends, onSelect: (dateText) => {
 			selectedDate = new Date(dateText)
+			$('.booking-times__wrapper').removeClass('d-none')
+			bookingErrors.text('')
 			disableTimes()
 			processDates(baseUrl, selectedDate)
 		}
