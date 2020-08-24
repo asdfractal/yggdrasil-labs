5a5440abd06bccec473d6834574db4b1765f251d
James Gregory <james.fractal@protonmail.com>
2020-08-24 23:12:31 +1000

    add missing docstring

--- api/resources.py
+++ api/resources.py
@@ -4,6 +4,10 @@ from bookings.models import Booking
 
 
 class BookingResource(ModelResource):
+    """
+    Queries the Booking model and filters by date.
+    """
+
     class Meta:
         queryset = Booking.objects.all()
         resource_name = "booking"
