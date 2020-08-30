0498748c9a6baab670f01a7355136608d2fc108e
James Gregory <james.fractal@protonmail.com>
2020-08-30 16:04:27 +1000

    add docstring

--- yggdrasil_labs/utils/check_internet_explorer.py
+++ yggdrasil_labs/utils/check_internet_explorer.py
@@ -1,4 +1,7 @@
 def check_internet_explorer(request):
+    """
+    Check if the browser is internet explorer and return boolean.
+    """
     head = request.META["HTTP_USER_AGENT"]
     if "Trident" in head:
         return True
