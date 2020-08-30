b08c87a67095cb3a8c50de409bab289992e45076
James Gregory <james.fractal@protonmail.com>
2020-08-30 16:03:01 +1000

    add discontinued field to product to work with lineitem PROTECT method

--- products/models.py
+++ products/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     featured = models.BooleanField(default=False, null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    discontinued = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
